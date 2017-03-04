55a4cc4a47022dd698f21376566b5fde8b0401fa
reaganwei0216 <reaganwei0216@gmail.com>
2017-03-04 18:38:25 +0800

    hw2

---
+++ CS280-Homework-1.2/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Pokemon
+    {
+        public float height;
+        public float weight;
+        public string Category;
+        public string Abilities;
+        public string Gender;
+    }
+}
---
+++ CS280-Homework-1.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon();
+            pokemon.height = 5.03f;
+            pokemon.weight = 80.7f;
+            pokemon.Category = "Arrow Quill";
+            pokemon.Abilities = "Overgrow";
+            pokemon.Gender = "unknown";
+            Console.WriteLine("Height:{0}meters, Weight:{1}tons, Category:{2}, Ability:{3}, Gender:{4}", pokemon.height, pokemon.weight, pokemon.Category, pokemon.Category, pokemon.Gender);
+        }
+    }
+}
